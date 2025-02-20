894ea817738155792f0e693dbe78a5d1906189ae
khaled88r5 <khaled88r5@gmail.com>
2025-02-20 15:37:33 +0200

    session 4

--- session4/problemsolving.dart
+++ session4/problemsolving.dart
@@ -1,10 +1,3 @@
-/* 
- Custom Message Function
- Topic: Optional and Named Parameters
- Exercise: Write a function displayMessage that takes a required String message, an optional String name, and a named
- int repeat with a default value of 1. The function should print the message and the name repeat times. If name is not
- provided, it should print the message alone
-*/
 void main() {
   bool dession = false;
   List<int> my = [5, 8, 10, 6, 7, 12];
